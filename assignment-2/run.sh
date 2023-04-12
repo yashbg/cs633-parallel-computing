44b2a301495aa2fa32f62a17687dfd466659ded3
Yash Gupta <yashbg7@gmail.com>
2023-04-12 16:13:41 +0530

    Correct node numbers

--- assignment-2/run.sh
+++ assignment-2/run.sh
@@ -3,20 +3,20 @@ rm -f times.txt
 
 for i in $(seq 1 5)
 do 
-	mpirun -np 4 -hosts csews7:4 ./code 2 2 4096
+	mpirun -np 4 -hosts csews25:4 ./code 2 2 4096
 done
 
 for i in $(seq 1 5)
 do 
-	mpirun -np 8 -hosts csews7:4,csews24:4 ./code 2 4 4096
+	mpirun -np 8 -hosts csews25:4,csews32:4 ./code 2 4 4096
 done
 
 for i in $(seq 1 5)
 do 
-	mpirun -np 4 -hosts csews7:4 ./code 2 2 32768
+	mpirun -np 4 -hosts csews25:4 ./code 2 2 32768
 done
 
 for i in $(seq 1 5)
 do 
-	mpirun -np 8 -hosts csews7:4,csews24:4 ./code 2 4 32768
+	mpirun -np 8 -hosts csews25:4,csews32:4 ./code 2 4 32768
 done
