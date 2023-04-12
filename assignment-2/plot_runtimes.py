98adae0c521e9e31096590913aededff1051630c
akshatsh49 <akshat.iitkanpur@gmail.com>
2023-04-12 16:11:13 +0530

    Updated plotting script to make plots for size = 4096

--- assignment-2/plot_runtimes.py
+++ assignment-2/plot_runtimes.py
@@ -19,15 +19,22 @@ if __name__ == '__main__':
         
         add_entry(runtimes1D, px, py, size, runtime1D)
         add_entry(runtimes2D, px, py, size, runtime2D)
-
+    
     boxplot_1d_runtimes = []
     boxplot_2d_runtimes = []
+    boxplot_small_1d = []
+    boxplot_small_2d = []
     for i, (config, val) in enumerate(runtimes1D.items()):
         boxplot_1d_runtimes.append(val)
+        if config[-1] == 4096:
+            boxplot_small_1d.append(val)    # for just size 4096
     
     for i, (config, val) in enumerate(runtimes2D.items()):
         boxplot_2d_runtimes.append(val)
+        if config[-1] == 4096:
+            boxplot_small_2d.append(val)    # for just size 4096
     
+    # plot of all values 
     bp1 = plt.boxplot(boxplot_1d_runtimes, positions=[1,2,1,2], patch_artist=True, boxprops=dict(facecolor="C0"))
     bp2 = plt.boxplot(boxplot_2d_runtimes, positions=[1,2,1,2], patch_artist=True, boxprops=dict(facecolor="C2"))
     
@@ -39,3 +46,20 @@ if __name__ == '__main__':
     plt.tight_layout()
     
     plt.savefig('./runtimes.png', dpi=300)
+    plt.close()
+    
+    # plot of just size=4096 values 
+    bp1 = plt.boxplot(boxplot_small_1d, positions=[1,2], patch_artist=True, boxprops=dict(facecolor="C0"))
+    bp2 = plt.boxplot(boxplot_small_2d, positions=[1,2], patch_artist=True, boxprops=dict(facecolor="C2"))
+    
+    plt.xticks(ticks = range(3), labels = ["", (2,2), (2,4)], rotation = 30)  # required to fix label alignment issue
+    plt.xlabel("Configurations")
+    plt.ylabel("Runtime in seconds")
+    plt.legend([bp1["boxes"][0], bp2["boxes"][0]], ['1D', '2D'], bbox_to_anchor = (1,1))
+    plt.title("Configuration Vs Runtime for data size = 4096")
+    plt.tight_layout()
+    
+    plt.savefig('./runtimes_size_4096.png', dpi=300)
+    plt.close()
+    
+    
